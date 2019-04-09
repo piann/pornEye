f11fd60a2e79fefee5a8d19d16beaab062f29220
root <root@localhost.localdomain>
2019-04-09 08:15:24 -0400

    fix misuse : convert into name_scope

--- model.py
+++ model.py
@@ -18,27 +18,27 @@ class Model:
         self.Y = tf.placeholder(tf.float32, shape=[None, 2])
         X_img = tf.reshape(self.X, [-1, RESIZED_HEIGHT, RESIZED_WIDTH, 3])
         
-        with tf.variable_scope("layer#1"):
+        with tf.name_scope("layer#1"):
             conv1 = tf.layers.conv2d(inputs=X_img, filters=32, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
             pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], padding="SAME", strides=2)
             dropout1 = tf.layers.dropout(inputs=pool1, rate=0.5, training=self.isTraining)
 
-        with tf.variable_scope("layer#2"):
+        with tf.name_scope("layer#2"):
             conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
             pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], padding="SAME", strides=2)
             dropout2 = tf.layers.dropout(inputs=pool2, rate=0.5, training=self.isTraining)
 
-        with tf.variable_scope("layer#3"):
+        with tf.name_scope("layer#3"):
             conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3, 3], padding="SAME", activation=tf.nn.relu)
             pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], padding="SAME", strides=2)
             dropout3 = tf.layers.dropout(inputs=pool3, rate=0.5, training=self.isTraining)
 
-        with tf.variable_scope("layer#4"):
+        with tf.name_scope("layer#4"):
             flat = tf.reshape(dropout3, [-1, 128 * 4 * 4])
             dense4 = tf.layers.dense(inputs=flat, units=625, activation=tf.nn.relu)
             dropout4 = tf.layers.dropout(inputs=dense4, rate=0.5, training=self.isTraining)
 
-        with tf.variable_scope("output"):
+        with tf.name_scope("output"):
             self.logits = tf.layers.dense(inputs=dropout4, units=2)
         
 
